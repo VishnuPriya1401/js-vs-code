8f8f7d738eadd7813c989b6d87290718aacf99ad
VishnuPriya1401 <eakpriya14@gmail.com>
2025-05-15 12:42:55 +0530

    linked files updated

--- js-files/test_1.js
+++ js-files/test_1.js
@@ -1,1 +1,6 @@
 console.log("test js file created");
+
+function changeText(){
+    document.querySelector(".test-case-1").innerHTML="text changed"
+    console.log("button clicked - text change - text generated")
+}
