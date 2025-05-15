b00ef866782acec1c4d2e84f434cb62d24ea1c36
VishnuPriya1401 <eakpriya14@gmail.com>
2025-05-15 13:35:44 +0530

    rock-paper-scissor-game-trail

--- js-files/test_1.js
+++ js-files/test_1.js
@@ -4,3 +4,8 @@ function changeText(){
     document.querySelector(".test-case-1").innerHTML="text changed"
     console.log("button clicked - text change - text generated")
 }
+
+function runTest(player){
+    document.querySelector(".result-text").innerHTML=player;
+    console.log(player)
+}
